9168f0d823931fd527a9d42f7c7d457daf712789
LuminescentOrange <qiyuanc3@illinois.edu>
2022-11-15 17:57:35 -0600

    add main screen and new header

--- src/components/GroupName.js
+++ src/components/GroupName.js
@@ -25,7 +25,6 @@ function GroupName({ groupName }) {
               component="div"
               sx={{
                 flexGrow: 1,
-                // color: 'black'
               }}
             >
               {groupName}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -38,7 +38,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
