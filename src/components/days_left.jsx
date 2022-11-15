94a6cfb48aa9d2527a55ee29b8ca06ccdb811377
josephjkuang <josephjkuang@gmail.com>
2022-11-15 02:32:30 -0600

    Days left text

---
+++ src/components/days_left.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function Days_Left() {
+  return (
+    <center>
+    <h3 sx={{display: 'inline'}}>
+        You have 
+        <Box
+            sx={{
+            width: 5 / 100,
+            border: 3,
+            boxShadow: 10,
+            px: 0.5,
+            mx: 1,
+            display: 'inline',
+            // backgroundColor: 'primary.dark',
+            '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+            },
+            }}
+        >
+        2
+      </Box>
+      days left to respond
+    </h3>
+    </center>
+
+  );
+}
