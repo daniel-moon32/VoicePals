d3bbee910389c7aa6a10a35da466bc4551a47826
LuminescentOrange <qiyuanc3@illinois.edu>
2022-11-16 16:29:55 -0600

    add a return button on the GroupName bar

--- src/components/GroupName.js
+++ src/components/GroupName.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Container, createTheme } from '@mui/material';
+import { Box, createTheme } from '@mui/material';
 import { ThemeProvider } from '@mui/system';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function GroupName({ groupName }) {
   const theme = createTheme({
@@ -17,19 +18,29 @@ function GroupName({ groupName }) {
   return (
     <ThemeProvider theme={theme}>
       <AppBar position="sticky" sx={{ bgcolor: '#c8b0bd' }}>
-        <Toolbar style={{ minHeight: '45px' }}>
-          <Container>
-            <Typography
-              align="center"
-              variant="h6"
-              component="div"
-              sx={{
-                flexGrow: 1,
-              }}
+        <Toolbar style={{ minHeight: '50px' }}>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <ArrowBackIcon
+              size="large"
+              aria-label="show more"
+              aria-controls={() => {}}
+              aria-haspopup="true"
+              onClick={() => {}}
+              color="inherit"
             >
-              {groupName}
-            </Typography>
-          </Container>
+              <ArrowBackIcon />
+            </ArrowBackIcon>
+          </Box>
+          <Typography
+            align="center"
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            {groupName}
+          </Typography>
         </Toolbar>
       </AppBar>
     </ThemeProvider>
