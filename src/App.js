ac7f5549651bb635604dfb844904b15710d2a043
LuminescentOrange <qiyuanc3@illinois.edu>
2022-11-16 20:46:26 -0600

    fix the conflict

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import AskQuestion from './pages/askQuestion';
+import MainScreen from './pages/MainScreen';
 
 function App() {
   return (
