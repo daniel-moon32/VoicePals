aec362ceccbedf0dad040fd6f50fc5a467af8968
Qiyuan Cheng <95161363+LuminescentOrange@users.noreply.github.com>
2022-11-17 14:22:35 -0600

    Wait replying (#25)
    * Audios
    
    * fix conflict
    
    Co-authored-by: josephjkuang <josephjkuang@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-
 import { Routes, Route } from 'react-router-dom';
 import AskQuestion from './pages/askQuestion';
 import MainScreen from './pages/MainScreen';
 import Replying from './pages/Replying';
 import CreateGroup from './pages/CreateGroup';
 import Listen from './pages/Listen';
+import WaitReplying from './pages/WaitReplying';
 
 function App() {
   return (
@@ -15,6 +14,7 @@ function App() {
       <Route path="/createGroup" element={<CreateGroup />} />
       <Route path="/answer/:groupid" element={<Replying />} />
       <Route path="/listen/:groupid" element={<Listen />} />
+      <Route path="/answer/wait/:groupid" element={<WaitReplying />} />
     </Routes>
   );
 }
--- src/components/AnswerPlayer.jsx
+++ src/components/AnswerPlayer.jsx
@@ -1,19 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import {
   styled, Typography, Slider,
-  Paper, Stack, Box,
+  Paper, Stack,
 } from '@mui/material';
 
-import Avatar from '@mui/material/Avatar';
 import PauseIcon from '@mui/icons-material/Pause';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
-import fade from '../audio/As You Fade Away - NEFFEX.mp3';
-import Josh from '../images/Josh.png';
-import ConvertText from './ConvertText';
-
 const CustomPaper = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#F5F5F5',
+  backgroundColor: '#F0F0F0',
   marginLeft: theme.spacing(4),
   marginRight: theme.spacing(4),
   marginBottom: theme.spacing(1.5),
@@ -33,15 +28,8 @@ const PSlider = styled(Slider)(({ theme, ...props }) => ({
   },
 }));
 
-const playlist = [fade];
-
-export default function QuestionPlayer() {
+export default function AnswerPlayer({ currentSong }) {
   const audioPlayer = useRef();
-
-  const [index] = useState(0);
-  const [hasText, setHasText] = useState(false);
-
-  const [currentSong] = useState(playlist[index]);
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume] = useState(30);
@@ -90,19 +78,7 @@ export default function QuestionPlayer() {
   return (
     <>
       <audio src={currentSong} ref={audioPlayer} muted={mute} />
-      <CustomPaper>
-        <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-          <Stack direction="row" spacing={1} justifyContent="center" sx={{ pb: 0.1 }}>
-            <Avatar
-              alt="Josh Belser"
-              src={Josh}
-              sx={{
-                boxShadow: 5, width: 36, height: 36,
-              }}
-            />
-            <Typography variant="h5">Joshua</Typography>
-          </Stack>
-        </Box>
+      <CustomPaper sx={{ width: 0.6 }}>
         <Stack
           spacing={1}
           direction="row"
@@ -110,6 +86,7 @@ export default function QuestionPlayer() {
             display: 'flex',
             alignItems: 'center',
             pr: 0,
+            left: true,
           }}
         >
           {!isPlaying
@@ -118,33 +95,8 @@ export default function QuestionPlayer() {
           <Typography sx={{ color: '#777490' }}>{formatTime(elapsed)}</Typography>
           <PSlider thumbless value={elapsed} max={duration} />
           <Typography sx={{ color: '#777490' }}>{formatTime(duration - elapsed)}</Typography>
-          <ConvertText setHasText={setHasText} />
         </Stack>
       </CustomPaper>
-      {hasText && (
-      <Box
-        sx={{
-          width: 80 / 100,
-          border: 1,
-          borderRadius: 1,
-          boxShadow: 4,
-          '&:hover': {
-            backgroundColor: 'primary.main',
-            opacity: [0.9, 0.8, 0.7],
-          },
-          mb: 2,
-        }}
-      >
-        <Typography style={{ overflowY: 'scroll', borderColor: 'red', borderWidth: 5 }} sx={{ height: 60, py: 0.5, px: 2 }}>
-          I know you told your friend you are not okay
-          and tell me what is wrong and why you never said you felt that way.
-          I guess you try to stay strong and fake a smile until I look away
-          but I have known you too long.
-          It hurts to watch your blue eyes fade to gray
-          as you fade away, as you fade away.
-        </Typography>
-      </Box>
-      )}
     </>
   );
 }
