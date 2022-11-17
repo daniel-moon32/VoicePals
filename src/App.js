321bce3240e64dd87817268e13e4224897bfea6f
josephjkuang <60304105+josephjkuang@users.noreply.github.com>
2022-11-16 18:18:19 -0600

    Replying (#19)
    * New branch
    
    * Audio bar
    
    * New audio box
    
    * Centered
    
    * New header
    
    * Finished replying for now
    
    * Stupid fucking linter
    
    * Fixed mistake in merge conflict

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import {
-  Routes, Route,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AskQuestion from './pages/askQuestion';
 import MainScreen from './pages/MainScreen';
 
 function App() {
   return (
-    // <AskQuestion />
     <Routes>
       <Route path="/" element={<MainScreen />} />
       <Route path="/askQuestion" element={<AskQuestion />} />
     </Routes>
-
   );
 }
 
---
+++ src/pages/Replying.js
@@ -0,0 +1,21 @@
+import DaysLeft from '../components/DaysLeft';
+import GroupName from '../components/GroupName';
+import SendButton from '../components/SendButton';
+import TimeTravel from '../components/TimeTravel';
+import QuestionAudio from '../components/QuestionAudio';
+
+export default function Replying() {
+  return (
+    <>
+      <GroupName groupName="Long Live Pinto Bean" />
+      <br />
+      <QuestionAudio />
+      <br />
+      <TimeTravel />
+      <br />
+      <DaysLeft />
+      <br />
+      <SendButton />
+    </>
+  );
+}
