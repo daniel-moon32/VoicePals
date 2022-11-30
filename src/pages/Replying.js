b9c0b2dc754d70fdd331716e54d36742a734bab2
Qiyuan Cheng <95161363+LuminescentOrange@users.noreply.github.com>
2022-11-30 11:32:23 -0600

    add no answer error handle and fix the audio left shift (#39)
    * Audios
    
    * fix conflict
    
    * add back button and submit error
    
    * add back button in the create group page
    
    * handle error for empty answer and fix the left shift of the audio
    
    Co-authored-by: josephjkuang <josephjkuang@gmail.com>

--- src/pages/Replying.js
+++ src/pages/Replying.js
@@ -1,5 +1,10 @@
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
+import { createTheme, Grid } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
 import DaysLeft from '../components/DaysLeft';
 import GroupName from '../components/GroupName';
 import SendButton from '../components/SendButton';
@@ -8,28 +13,90 @@ import QuestionAudio from '../components/QuestionAudio';
 import Record from '../components/Record';
 
 export default function Replying({ data }) {
+  const navigate = useNavigate();
   const { groupid } = useParams();
-  const [blobURL, setBlobURL] = useState('');
-  const [question, setQuestion] = useState('');
+  const [ansBlobURL, setAnsBlobURL] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(false);
+  const [errorContent, setErrorContent] = useState('');
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#4682B4',
+      },
+      secondary: {
+        main: '#2C2F33',
+      },
+    },
+    typography: {
+      fontFamily: [
+        'sans-serif',
+      ].join(','),
+    },
+  });
+
+  function handleSubmit() {
+    if (answer === '' && ansBlobURL === '') {
+      setError(true);
+      setErrorContent('Please enter the answer');
+      return;
+    }
+    navigate(`/answer/wait/${groupid}`);
+  }
 
   return (
     <>
-      <GroupName groupName={data[groupid - 1].group_name} />
-      <br />
-      <QuestionAudio question={data[groupid - 1].question} />
-      <hr style={{ width: '92%' }} />
-      <br />
-      <TimeTravel peopleReplying={data[groupid - 1].responses.length} />
-      <br />
-      <DaysLeft days={data[groupid - 1].days_left} />
-      <br />
-      <Record
-        blobURL={blobURL}
-        setBlobURL={setBlobURL}
-        question={question}
-        setQuestion={setQuestion}
-      />
-      <SendButton />
+      {error && (<Alert severity="error">{errorContent}</Alert>)}
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        rowSpacing={2}
+        paddingTop={2}
+      >
+        <GroupName groupName={data[groupid - 1].group_name} />
+
+        <Grid item width={390} marginTop={2}>
+          <QuestionAudio question={data[groupid - 1].question} />
+        </Grid>
+        <Grid item width={390}>
+          <hr style={{ width: '92%' }} />
+        </Grid>
+        <Grid item width={390}>
+          <TimeTravel peopleReplying={data[groupid - 1].responses.length} />
+        </Grid>
+        <Grid item>
+          <DaysLeft days={data[groupid - 1].days_left} />
+        </Grid>
+
+        <Grid item>
+          <Record
+            blobURL={ansBlobURL}
+            setBlobURL={setAnsBlobURL}
+            question={answer}
+            setQuestion={setAnswer}
+          />
+
+        </Grid>
+
+        {/* <SendButton /> */}
+        <Grid item>
+          <ThemeProvider theme={theme}>
+            <Button
+              variant="contained"
+              sx={{
+                width: 200, padding: 1, margin: 2, color: '#ffffff', bgcolor: '#4682B4',
+              }}
+              onClick={() => handleSubmit()}
+            >
+              Send
+            </Button>
+          </ThemeProvider>
+
+        </Grid>
+
+      </Grid>
     </>
   );
 }
